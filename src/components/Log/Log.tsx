91b1e7adac2695dfdffce14cc70ad78c73eeae52
MahmoodSeoud <Mahmoodseoud@gmail.com>
2023-12-14 19:03:30 +0100

    styling

--- src/components/Log/Log.tsx
+++ src/components/Log/Log.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { LogHistory } from '../../App';
 import { Button } from 'primereact/button';
 import 'primeicons/primeicons.css';
+import './Log.css';
 
 interface ILogProps {
     log: LogHistory;
@@ -12,16 +13,16 @@ export default function Log({ log }: ILogProps) {
     const [visible, setVisible] = useState(false);
     return (
         <div className="card flex justify-content-center">
-
             <Sidebar
                 visible={visible}
                 onHide={() => setVisible(false)}
                 position="right"
-            >
+                >
+                <h1>Log</h1>
                 {log.logEntries && log.logEntries.length > 0 && log.logEntries.map((logEntry, index) => (
-                    <div key={index}>
-                        <p>Address: {logEntry.address}</p>
-                        <p>Cache hit? {logEntry.hit}</p>
+                    <div className='log' key={index}>
+                        <p>Address: {logEntry.address.toString(2).padStart(14,'0')}</p>
+                        <p>Cache hit? {logEntry.hit.toString()}</p>
                     </div>
                 ))}
 
