a444d114d4e800756ad34d6bf0fe80b7fcc7364a
MahmoodSeoud <Mahmoodseoud@gmail.com>
2023-12-18 14:10:46 +0100

    cleaner table

--- src/components/Cache_visual_table/Cache_visual_table.tsx
+++ src/components/Cache_visual_table/Cache_visual_table.tsx
@@ -14,31 +14,35 @@ function Cache_visual_table({ cache, tag, changedSet, changedLine }: cache_table
         <div>
             <h2>Cache</h2>
             <table className='cache-table'>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Valid</th>
-                        <th>Tag</th>
-                        <th>Block</th>
-                    </tr>
-                </thead>
+
                 <tbody>
                     {cache.sets && cache.sets.map((set, i) => {
                         const changedSet_ = i === changedSet;
                         return (
                             <tr key={i}>
-                                <td>Set {i}</td>
+                                <th>Set {i}</th>
                                 <td colSpan={3}>
-                                        {set.lines && set.lines.length > 0 && set.lines.map((line, j) => {
-                                            const blockClass = j === changedLine ? 'changed-block' : '';
-                                            return (
-                                                <tr key={j}>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Valid</th>
+                                                <th>Tag</th>
+                                                <th>Block</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {set.lines && set.lines.length > 0 && set.lines.map((line, j) => {
+                                                const blockClass = j === changedLine ? 'changed-block' : '';
+                                                return (
+                                                    <tr key={j}>
                                                         <td className={changedSet_ ? blockClass : ''}>{line.valid}</td>
                                                         <td className={changedSet_ ? blockClass : ''}>{line.tag.toString(2).padStart(tag, '0')}</td>
                                                         <td className={changedSet_ ? blockClass : ''}>{line.blockSizeStr}</td>
-                                                </tr>
-                                            )
-                                        })}
+                                                    </tr>
+                                                )
+                                            })}
+                                        </tbody>
+                                    </table>
                                 </td>
                             </tr>
                         )
